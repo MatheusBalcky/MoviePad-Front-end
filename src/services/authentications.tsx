4cf413f8f72d5e5fddf9cfe440385fcabe4c4ef1
Matheus Balcky <matheuscabal821@gmail.com>
2022-10-03 15:21:34 -0300

    FEAT: Functions to requesitions and components more clean

---
+++ src/services/authentications.tsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export interface signUpData {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+export type signInData = Omit<signUpData, 'passwordConfirm'>
+
+export async function signUp(signUpData: signUpData) {
+  const urlApi = `${import.meta.env.VITE_API_URL}/signup`;
+
+  const promise = axios.post(urlApi, signUpData);
+  return promise;
+}
+
+export async function signIn(signInData: signInData) {
+  const urlApi = `${import.meta.env.VITE_API_URL}/signin`;
+
+  const promise = axios.post(urlApi, signInData);
+  return promise;
+}
