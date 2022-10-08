f8735d9cc2ae23c04a92e31319f2af172387fde1
Matheus Balcky <matheuscabal821@gmail.com>
2022-10-08 15:28:21 -0300

    👌 IMPROVE: 📦 NEW:  Refactoring and new service to add new content

--- src/services/lists.tsx
+++ src/services/lists.tsx
@@ -1,33 +1,31 @@
 import axios from 'axios';
 const urlApi = import.meta.env.VITE_API_URL;
+import { headersToken } from './authentications';
 
 export async function getLists(token: string) {
-  const promise = axios.get(`${urlApi}/lists`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-
-  return promise;
+  return axios.get(`${urlApi}/lists`, headersToken(token));
 }
 
 export async function createList(token: string, listData: any) {
-  const bodyToken = {
-    headers: { Authorization: `Bearer ${token}` },
-  };
-  return axios.post(`${urlApi}/lists/create`, listData, bodyToken);
+  return axios.post(`${urlApi}/lists/create`, listData, headersToken(token));
 }
 
 export async function getOneListAndContents(token: string, listId: number) {
-  const promise = axios.get(`${urlApi}/lists/${listId}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-
-  return promise;
+  return axios.get(`${urlApi}/lists/${listId}`, headersToken(token));
 }
 
 export async function deleteList(token: string, listId: number) {
-  const promise = axios.delete(`${urlApi}/lists/${listId}/remove`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  return axios.delete(`${urlApi}/lists/${listId}/remove`, headersToken(token));
+}
 
-  return promise;
+export async function addNewContent(
+  token: string,
+  listId: number,
+  content: object
+) {
+  return axios.post(
+    `${urlApi}/lists/${listId}/addcontent`,
+    content,
+    headersToken(token)
+  );
 }
